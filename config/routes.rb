4ed8c2eae6dfcd16aee2a317317f24332f256ed6
KatsuyaHiraki <112613868+hirakikatsuya@users.noreply.github.com>
2022-11-03 11:21:50 +0000

    グループ参加・離脱機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
 
   resources :chats,only:[:show,:create]
 
-  resources:groups,except:[:destroy]
+  resources:groups do
+    get "join" => "groups#join"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
